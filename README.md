ef29334666fadf4e68767c86317749f49114f28f
Spiikki Sillanpää <spiikki@nalleperhe.net>
2024-01-13 21:56:34 +0000

    fixed bugs, added sauna mode, added configuration file

--- README.md
+++ README.md
@@ -10,4 +10,30 @@ RaspberryPi, NeoPixel led-strip
 
 ## Usage
 
-Check client.py for simple example.
+Copy settings.py.dist to settings.py and configure your mqtt broker settings.
+Topics used in client require settings for light and sauna. 
+
+### Setting lighting scene
+
+Topic 'light/1'
+'''
+{
+  "cmd": "set",
+  "buffer": [
+    [0, 0, 255],
+    [255, 0, 0]
+  ],
+  "rotation" : {
+    "enable": "True",
+    "speed": 0.2
+  }
+}
+'''
+Set rotating two color tween over whole strip
+
+
+'''
+{ "cmd" : "sauna" }
+'''
+Set sauna-mode on.
+
--- client.py
+++ client.py
@@ -3,59 +3,78 @@ from lightAdapter import *
 from lightBuffer import *
 import paho.mqtt.client as mqtt
 from time import sleep
+import settings
 
 hal = lightAdapter()
 buffer = lightBuffer()
-rotation = False
 rotation_speed = 0.04
+state = "sauna"
 
 def mqtt_on_connect(client, userdata, flags, rc):
     print("Connected! : "+str(rc))
-    client.subscribe("light/1/#")
+    client.subscribe(settings.mqtt_light_topic + "/#")
+    client.subscribe(settings.mqtt_sauna_topic)
 
 def mqtt_on_message(client, userdata, msg):
+    global rotation_speed
+    global state
     print(msg.topic + " : ")
-    command = jsons.loads(msg.payload)
-    print(command)
-    tween_size=1
-    if command["cmd"] == "set":
-        if "tween_size" in command:
-            tween_size=command["tween_size"]
-        if len(command["buffer"]) < 2:
-            buffer.set(command["buffer"][0])
-        else:
-            if (len(command["buffer"])*tween_size) < 30:
-                tween_size = int(30/(len(command["buffer"])-1))+1
-            buffer.clearBuffer()
-            for set in range(0, len(command["buffer"])-1):
-                buffer.tween(command["buffer"][set], command["buffer"][set+1], tween_size)
-        if "rotation" in command:
-            if "enable" in command["rotation"]:
-                if command["rotation"]["enable"] == "True":
-                    rotation = True
-                else:
-                    rotation = False
-            if "speed" in command["rotation"]:
-                rotation_speed = command["rotation"]["speed"]
+    try:
+        command = jsons.loads(msg.payload)
+        print(command)
+        tween_size=1
+        if "cmd" in command and command["cmd"] == "set":
+            state = "light"
+            if "tween_size" in command:
+                tween_size=command["tween_size"]
+            if len(command["buffer"]) < 2:
+                buffer.set(command["buffer"][0])
+            else:
+                if (len(command["buffer"])*tween_size) < 30:
+                    tween_size = int(30/(len(command["buffer"])-1))+1
+                buffer.clearBuffer()
+                for set in range(0, len(command["buffer"])-1):
+                    buffer.tween(command["buffer"][set], command["buffer"][set+1], tween_size)
+            if "rotation" in command:
+                if "enable" in command["rotation"]:
+                    if command["rotation"]["enable"] == "True":
+                        buffer.toggleRotation(True)
+                    else:
+                        buffer.toggleRotation(False)
+                if "speed" in command["rotation"]:
+                    rotation_speed = command["rotation"]["speed"]
+        if "cmd" in command and command["cmd"] == "sauna":
+            state = "sauna"
+        elif "sauna" in msg.topic:
+            if "temperature" in command:
+                print(command["temperature"])
+                green = 255-(320*(command["temperature"]/100)-17)
+                if green > 255:
+                    green = 255
+                elif green < 0:
+                    green = 0
+
+                if state == "sauna":
+                    buffer.toggleRotation(False)
+                    if command["temperature"] < 6:
+                        buffer.set([55, 0, 255])
+                    else:
+                        buffer.set([255, int(green), 0])
+
+    except:
+        print("somethings fucked! don't care! let's go!")
 
 mqtt_client = mqtt.Client()
 mqtt_client.on_connect = mqtt_on_connect
 mqtt_client.on_message = mqtt_on_message
-mqtt_client.connect("192.168.1.148", 1883, 30)
+mqtt_client.connect(settings.mqtt_host, settings.mqtt_port, 30)
 mqtt_client.loop_start()
-
-
-# buffer.tween((0,0,0),(0,255,0),10)
-# buffer.tween((0,255,0),(0,250,0),10)
-# buffer.tween((0,250,0),(0,250,0),10)
-# buffer.tween((0,250,0),(0,0,0),10)
 
 try:
     hal.start()
     while(1):
         hal.loopBuffer(buffer.getBuffer())
-        if rotation:
-            buffer.rotate(-1)
+        buffer.rotate(-1)
         sleep(rotation_speed)
 except KeyboardInterrupt:
     hal.stop()
